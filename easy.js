7b03edf028e7eb1370a789262c4a76fdf7158b15
RobbieKavanagh <130613407+RobbieKavanagh@users.noreply.github.com>
2024-05-20 10:12:49 -0230

    May 20 2024

---
+++ easy.js
@@ -0,0 +1,25 @@
+//Function to Play sound
+function playSound(soundFile) {
+    const audio = new Audio (soundFile);
+    audio.play();
+}
+
+//Event listener For C Button
+document.getElementById('string1').addEventListener('click', () => {
+    playSound('sounds/C.mp3'); 
+});
+
+//Event Listener for D Button
+document.getElementById('string2').addEventListener('click', () => {
+    playSound('sounds/D.mp3'); 
+});
+
+//Event Listener for E Button
+document.getElementById('string3').addEventListener('click', () => {
+    playSound('sounds/E.mp3'); 
+});
+
+//Event Listener for F Button
+document.getElementById('string4').addEventListener('click', () => {
+    playSound('sounds/F.mp3'); 
+});
