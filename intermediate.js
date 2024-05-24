282b5421134c2b60a819d5aea3bae50c99cdaf33
RobbieKavanagh <130613407+RobbieKavanagh@users.noreply.github.com>
2024-05-24 08:57:01 -0230

    May 24 2024

--- intermediate.js
+++ intermediate.js
@@ -3,3 +3,8 @@ function playSound(soundFile) {
     const audio = new Audio (soundFile);
     audio.play();
 }
+
+//Event listener For C Button
+document.getElementById('sound1').addEventListener('click', () => {
+    playSound('sounds/C.mp3'); 
+});
